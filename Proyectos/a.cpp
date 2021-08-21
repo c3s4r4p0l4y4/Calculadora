ec0014613213213a17944b43ab26ac7e6b149d1a
c3s4r4p0l4y4 <88946719+c3s4r4p0l4y4@users.noreply.github.com>
2021-08-21 02:53:40 -0500

    Add files via upload

---
+++ Proyectos/a.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    /*Escribir un programa que solicite el ingreso de la temperatura en grados, 
+    si la temperatura está por encima de 100 grados desplegar el mensaje “arriba 
+    del punto de ebullición del agua”, de lo contrario desplegar el mensaje 
+    “abajo del punto de ebullición del agua”.*/
+
+    int t=0; //t=temperatura
+    printf("Temperatura delete agu\n;");
+    printf("Ingrese la temperatura en grados: ");
+    scanf("%d",&t);
+
+    if(t>100){
+        printf("\narriba del punto de ebullición del agua");
+    }
+    else{
+        printf("\nabajo del punto de ebullición del agua");
+    }
+    
+    system("pause");
+    return 0;
+}
