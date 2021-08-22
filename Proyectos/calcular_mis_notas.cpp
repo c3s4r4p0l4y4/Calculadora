1a7725b3f32d7e1c1b8d60f1a89c5878d73b63ed
c3s4r4p0l4y4 <88946719+c3s4r4p0l4y4@users.noreply.github.com>
2021-08-21 22:18:53 -0500

    Update calcular_mis_notas.cpp

--- Proyectos/calcular_mis_notas.cpp
+++ Proyectos/calcular_mis_notas.cpp
@@ -6,15 +6,14 @@ using namespace std;
 
 int main(){
     printf("Bienvenido al programa que calcula el promedio de notas: ");
-    /*Tengo que tener variables dependiendo de las notas, el usuario me dara x notas
-    entonces */
+    /*I have to have variables depending on the notes, the user entered x notes, so*/
     float x,a,c=0,n;
     double avg;
     printf("\nIngrese la cantidad de notas: ");
     scanf("%f",&n);
     for(float i=0;i<=n;i++){
         printf("\nIngrese la nota: %f:",i+1);
-        scanf("%f",&a);  //el valor de a va a variar en cada ciclo
+        scanf("%f",&a);  //the value of "a" will be random in each loop
         c+=a;
     }
     avg=c/n;
